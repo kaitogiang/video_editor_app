0b7ebaf4de07f49f7051107b9861d35fb7670bc4
kaitogiang <huygiang001442@gmail.com>
2024-12-25 09:10:51 +0700

    Fix the error when initializing video editor controller

--- lib/video_editor/views/home_screen/home_screen.dart
+++ lib/video_editor/views/home_screen/home_screen.dart
@@ -1,15 +1,61 @@
 import 'dart:developer';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:video_editor/video_editor.dart';
 import 'package:video_editor_app/video_editor/utils/shared_method.dart';
 import 'package:video_editor_app/video_editor/widgets/add_text_form.dart';
 import 'package:video_editor_app/video_editor/widgets/bottom_modal.dart';
 import 'package:video_editor_app/video_editor/widgets/select_options.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  File? file;
+  final ValueNotifier<File?> _selectedFile = ValueNotifier(null);
+  late VideoEditorController _videoEditorController;
+
+  @override
+  void initState() {
+    super.initState();
+    // _controller.initialized;
+    // _controller
+    //     .initialize(aspectRatio: 9 / 16)
+    //     .then((_) => setState(() {}))
+    //     .catchError((error) {
+    //   Navigator.pop(context);
+    // }, test: (e) => e is VideoMinDurationError);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _videoEditorController.dispose();
+  }
+
+  void _initializeVideoEditorController(File file) {
+    log('File is ${file.path}');
+    _videoEditorController = VideoEditorController.file(
+      file,
+      minDuration: const Duration(seconds: 1),
+      maxDuration: const Duration(seconds: 10),
+    );
+    _videoEditorController
+        .initialize(aspectRatio: 9 / 16)
+        .then((_) => setState(() {}))
+        .catchError((error) {
+      Navigator.pop(context);
+    }, test: (e) => e is VideoMinDurationError);
+
+    log('Video editor initialize in the method: ${_videoEditorController.initialized}');
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +66,8 @@ class HomeScreen extends StatelessWidget {
           IconButton(
             onPressed: () {
               log('Saving the edited video');
-              _buildOptionDialog(context);
+              // _buildOptionDialog(context, file);
+              log('Selected file is: ${file?.path}');
             },
             icon: const Icon(Icons.save),
           ),
@@ -39,11 +86,19 @@ class HomeScreen extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Expanded(
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: theme.colorScheme.secondary,
-                  ),
-                ),
+                child: ValueListenableBuilder(
+                    valueListenable: _selectedFile,
+                    builder: (context, selectedFile, child) {
+                      return Container(
+                        decoration: BoxDecoration(
+                          color: theme.colorScheme.secondary,
+                        ),
+                        // child: (selectedFile != null)
+                        //     ? Image.file(selectedFile)
+                        //     : Container(),
+                        child: Container(),
+                      );
+                    }),
               ),
               Expanded(
                 child: Container(
@@ -63,10 +118,32 @@ class HomeScreen extends StatelessWidget {
                 //The button for showing the options
                 ActionButton(
                   svgIconPath: 'assets/icons/plus_icon.svg',
-                  onPressed: () {
+                  onPressed: () async {
                     log('Open the options');
                     //Showing the bottom sheet to show the options
-                    _buildOptionDialog(context);
+                    final file = await _buildOptionDialog(context) as File?;
+                    log('file in ActionButton: ${file?.path}');
+                    _selectedFile.value = file;
+                    //if the selected file is not null, then initialize the video editor controller
+                    if (file != null) {
+                      // _initializeVideoEditorController(file);
+                      _videoEditorController = VideoEditorController.file(
+                        file,
+                        minDuration: const Duration(seconds: 1),
+                        maxDuration: const Duration(seconds: 10),
+                      );
+
+                      await _videoEditorController
+                          .initialize(aspectRatio: 9 / 16)
+                          .then((_) => setState(() {
+                                log('Calling setState when initializing video Controller');
+                              }))
+                          .catchError((error) {
+                        log('Error initializing video editor: $error');
+                        Navigator.pop(context);
+                      }, test: (e) => e is VideoMinDurationError);
+                      log('Video editor controller is initialized: ${_videoEditorController.initialized}');
+                    }
                   },
                 ),
                 //The button for adding the text
@@ -102,18 +179,25 @@ class HomeScreen extends StatelessWidget {
     );
   }
 
-  Future<dynamic> _buildOptionDialog(BuildContext context) {
+  Future<dynamic> _buildOptionDialog(BuildContext context) async {
     return showBottomDialog(
       context: context,
       heightFactor: 0.71,
       child: SelectOptions(
         options: {
-          'Take photo': _takePhotoAction,
+          'Take photo': () async {
+            final file = await _takePhotoAction();
+            log('Selected file in buildOptionDialog: ${file?.path}');
+            Navigator.of(context, rootNavigator: true).pop(file);
+          },
           'Choose image from gallery': _selectImageFromGallery,
           'DreamWeaiver Gallery': () {
             log('DreamWeaiver Gallery');
           },
-          'Choose video from gallery': _selectVideoFromGallery,
+          'Choose video from gallery': () async {
+            final videoFile = await _selectVideoFromGallery();
+            Navigator.of(context, rootNavigator: true).pop(videoFile);
+          },
           'Record video': _recordVideo,
           'AI Images': () {
             log('AI Images');
@@ -125,14 +209,16 @@ class HomeScreen extends StatelessWidget {
 }
 
 //Methods for handling the selected option
-Future<void> _takePhotoAction() async {
+Future<File?> _takePhotoAction() async {
   log('Take photo from the camera directly');
   final picker = ImagePicker();
   final image = await picker.pickImage(source: ImageSource.camera);
   if (image != null) {
     //Do something here
     log('Image path: ${image.path}');
+    return File(image.path);
   }
+  return null;
 }
 
 Future<void> _selectImageFromGallery() async {
@@ -142,16 +228,19 @@ Future<void> _selectImageFromGallery() async {
   if (image != null) {
     //Do something here
     log('Image path in the gallery: ${image.path}');
+    final imageFile = File(image.path);
   }
 }
 
-Future<void> _selectVideoFromGallery() async {
+Future<File?> _selectVideoFromGallery() async {
   final picker = ImagePicker();
   final video = await picker.pickVideo(source: ImageSource.gallery);
   if (video != null) {
     //Do something here
     log('Video path in the gallery is: ${video.path}');
+    return File(video.path);
   }
+  return null;
 }
 
 Future<void> _recordVideo() async {
